956f9e6b6102150a63da6c577b03990c4f217e91
yoga-nugroho129 <yoga.nugroho129@gmail.com>
2020-05-16 11:46:41 +0700

    create separed routes, config, DB connection & models for DB

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const connectDB = require('./config/db') // connect with DB
 
 const app = express()
+
+// run the connectDB
+connectDB()
+
+// Init middleware in order to support express to get the body without express bodyParser
+app.use(express.json({ extended: false }))
 
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
